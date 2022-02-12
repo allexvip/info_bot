f32e6d0095e360c0c791292f27cafdafa6e99893
allexvip <alexey_shes@mail.ru>
2022-02-12 23:00:39 +0300

    fix feedback

--- handlers/other.py
+++ handlers/other.py
@@ -13,29 +13,34 @@ from aiogram.types.chat_member import ChatMember
 
 @dp.message_handler(lambda message: 'test' in message.text)
 async def taxi(message: types.Message):
-    answ = await bot.get_chat_member(chat_id=MAIN_CHANNEL_CHAT_ID, user_id=message.from_user.id)
-    print(answ['status'])
-    await message.answer(str(answ['status']))
-    # await message.delete()
-
+    try:
+        answ = await bot.get_chat_member(chat_id=MAIN_CHANNEL_CHAT_ID, user_id=message.from_user.id)
+        print(answ['status'])
+        await message.answer(str(answ['status']))
+        # await message.delete()
+    except Exception as e:
+        pass
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await send_sql(
-        "INSERT INTO logs (`chat_id`,`username`,`message`,`upd`) VALUES ('{0}','{1}','{2}',datetime('now'))".format(
-            message.chat.id,
-            message.chat.username,
-            message.text))
-    user_info = await bot.get_chat_member(chat_id=MAIN_CHANNEL_CHAT_ID, user_id=message.from_user.id)
-    answ_text = '@{} {}\n({} {} {})'.format(message.from_user.username, user_info['status'],
-                                            message.from_user.first_name,
-                                            message.from_user.last_name, message.from_user.id)
-    if len(message.text)<15:
-        await message.answer('Ничего не понял. Помощь /help')
-    else:
-        await bot.send_message(BOT_USER_ANSWERS_CHAT_ID, answ_text)
-        await message.send_copy(chat_id=BOT_USER_ANSWERS_CHAT_ID, disable_notification=True, reply_to_message_id=True)
-        await message.answer('Переслал Ваше сообщение модератору.\nСпасибо! \n\nПомощь /help')
+    try:
+        await send_sql(
+            "INSERT INTO logs (`chat_id`,`username`,`message`,`upd`) VALUES ('{0}','{1}','{2}',datetime('now'))".format(
+                message.chat.id,
+                message.chat.username,
+                message.text))
+        user_info = await bot.get_chat_member(chat_id=MAIN_CHANNEL_CHAT_ID, user_id=message.from_user.id)
+        answ_text = '@{} {}\n({} {} {})'.format(message.from_user.username, user_info['status'],
+                                                message.from_user.first_name,
+                                                message.from_user.last_name, message.from_user.id)
+        if len(message.text)<15:
+            await message.answer('Ничего не понял. Помощь /help')
+        else:
+            await bot.send_message(BOT_USER_ANSWERS_CHAT_ID, answ_text)
+            await message.send_copy(chat_id=BOT_USER_ANSWERS_CHAT_ID, disable_notification=True, reply_to_message_id=True)
+            await message.answer('Переслал Ваше сообщение модератору.\nСпасибо! \n\nПомощь /help')
+    except Exception as e:
+        pass
 
 
 def register_handlers_other(dp: Dispatcher):
