bd12b1eb9925d37619f572ea720a21481f078f4c
allexvip <alexey_shes@mail.ru>
2022-02-12 21:01:50 +0300

    fix

--- main.py
+++ main.py
@@ -20,22 +20,3 @@ other.register_handlers_other(dp)
 
 executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
 
-
-
-#
-# from aiogram import Bot, Dispatcher, executor, types
-# from aiogram.types.chat_member import ChatMember
-# from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-# from aiogram.utils.callback_data import CallbackData
-#
-# # logging.basicConfig(filename= LOGGING_FILE_NAME , encoding='utf-8', level=logging.DEBUG)
-# API_TOKEN = TELEGRAM_BOT_API_KEY
-#
-# # Configure logging
-# logging.basicConfig(level=logging.INFO)
-#
-# # Initialize bot and dispatcher
-# bot = Bot(token=API_TOKEN)
-# dp = Dispatcher(bot)
-# cur = con.cursor()
-# admin_chatid_list = [int(item) for item in ADMIN_CHAT_ID.split(',')]
