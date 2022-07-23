d077cc7f0c1e9ce127d5066a8adb09d4a971d8b0
allexvip <alexey_shes@mail.ru>
2022-07-23 20:33:12 +0300

    fix

---
+++ migrations/12_add_service_gov.sql
@@ -0,0 +1,16 @@
+INSERT INTO projects (`project_code`) VALUES ('alimentovergov');
+UPDATE projects
+SET
+`name`='🔥 Верхняя граница алиментов, как демографическая мера - пишем правительству.'
+,`DESC`='Тема обращения: Семья
+Повтор: Первичное
+Скачиваем приложения:
+
+👉 <a href="https://drive.google.com/file/d/14FnnbDJitnIX-6CnJrICBFgTof9fB0QR/view?usp=drivesdk">Приложение 1</a>
+
+👉 <a href="https://drive.google.com/file/d/1452BkW9usiwHiMWxgkuaf3aH1vkCDWwM/view?usp=drivesdk">Приложение 2</a>
+
+Обязательно приложите файлы к обращению.'
+WHERE  project_code ='alimentovergov';
+INSERT INTO `deps`` (`dep_full_name`, `dep`, `link`, `person_type`, `link_send`) VALUES ('Правительство России', 'Правительство России', 'http://services.government.ru/letters/form/', 'servicegov', 'http://services.government.ru/letters/form/');
+
